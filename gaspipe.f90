6285fad18a8f31ed3061b4f32cb1b91f49c54cf7
plmmc <45519216+plmmc@users.noreply.github.com>
2018-12-01 17:36:20 -0200

    Update gaspipe.f90

--- gaspipe.f90
+++ gaspipe.f90
@@ -6,36 +6,47 @@
         !    PEDRO LINS DE MOURA MARTINS DA COSTA    !
         !                                            !
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+        
+        
+        
         !!!!!!!!!!!!
         !PARÂMETROS!
         !!!!!!!!!!!!
         
-        integer, parameter :: n = 19 ! Valor a definir
+        
+        integer, parameter :: N = 20  !time steps Valor a definir
+        integer, parameter :: dx =  ! Valor a definir
+        real, parameter :: Ttot = ! Valor a definir
         real, parameter :: PI = 3.14159265359
         real, parameter :: g = 9.80665  !m/s²
-        real, parameter :: D = 10  !m A definir
+        real, parameter :: D = 0.07  !m A definir
+        real, parameter :: f = 0.002  ! pq?
+        real, parameter :: ro = 1.184 !kg/m³  (temp. 25º)
+        real, parameter :: c = 340.29 !m/s  (temp. 25º)
+        real, parameter :: teta = 0 !tubulação horizontal
+        
         
         !!!!!!!!!!!
         !VÁRIAVEIS!
         !!!!!!!!!!!
         
-        real :: p0, pL, L, zeta, f, D, ro, c, Q, S, a, b
-        real :: beta, alfa, dt, dx, 
-        integer :: i,j
+        real :: p0, pL, L, zeta, Q, S, a, b, dt
+        real :: beta, alfa, dx, 
+        integer :: i,j, N, M
         real, dimension (n,n) :: A, B
-
-        alfa = (16*f*Q)/((D**3)*(c**2)*PI)
-        beta = (2*g*sin(teta))/(c**2)
+        real, dimension :: x(:),g(:,:),asup(:),aprin(:),ainf(:), bcol(:),g_novo(0:)
+        
+        
+        
+        dt = Ttot/N
+        S = PI*((d/2.0)**2)
+        alfa = (16.0*f*Q)/((D**3.0)*(c**2.0)*PI)
+        beta = (2.0*g*sin(teta))/(c**2.0)
 
         a = dt/alfa*(dx)**2.0
         b = a*beta*dx/2.0
         zeta = (f/D)*((2.0*ro*c*Q/S)**2.0)
         
-
-        !Construção de malha uniforme
-        !dx = (b - a) / (N - 1)
-
         !!!!!!!!!!!!!!!!!!!!!!!
         !CONDIÇÕES DE CONTORNO!
         !!!!!!!!!!!!!!!!!!!!!!!
@@ -52,82 +63,23 @@
         !!!!!!!!!!!!!!!!!!
 
         do i=2, n-1
-        p(i) = 1
+           p(i) = 1
         end do
        
-        do i=2, n-1
-        x(i) = 0 + (i)*dx
-        p(i) = 
-        end do
-
-        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        !DIAGONAL DE COEFICIENTES DA MATRIX A!
-        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-        !preencher matrix com 0
-        do i = 1,n
-           do j = 1,n
-           A(i,j) = 0
-           endo do
-        end do
-
-        !diagonal principal
-        do i = 1,n
-          A(i,i) = (-2.0*a - 2.0)
-        end do
-
-        !diagonal inferior
-        do i = 2,n
-          A(i,i-1) = (a - b)
-        end do
-
-        !diagonal superior
-        do i = 2, n
-           A(i-1,i) = (a + b)
-        end do
+        !!!!!!!!!!!!!!!!!!!!!!!!
+        !RESOLVENDO AS MATRIZES!
+        !!!!!!!!!!!!!!!!!!!!!!!!
         
-        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        !DIAGONAL DE COEFICIENTES DA MATRIX B!
-        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-        !preencher matrix com 0
-        do i = 1,n
-           do j = 1,n
-           B(i,j) = 0
-           endo do
-        end do
-
-        !diagonal principal
-        do i = 1,n
-          B(i,i) = (2.0*a - 2.0)
-        end do
-
-        !diagonal inferior
-        do i = 2,n
-          B(i,i-1) = (-a + b)
-        end do
-
-        !diagonal superior
-        do i = 2, n
-           B(i-1,i) = (-a - b)
-        end do
-
-        !contruindo os coeficiente de conhecidos u, matrix k
-
-        !computando o valor da equação de pressão para o tempo desejado
-
-        !algoritmo de Thomas
-
-        !contruindo matrix d
-
-        !construindo c'
-
-        !construindo d'
-
-        !calculando
-
-        !condição de contorno de Neuman
-
-        !saída de data
+        do n = 0 , N !loop
+        do j = 1, JI-1  ! a,b,c are the coefficients of C-N scheme and d is the right part
+                a(j) = (a-b)
+                b(j) = -2.0-2.0*a
+                c(j) = (a+b)
+                d(j) = (-a-b)*g(n,j+1)+(2.0-2.0*a)*g(n,j)+(-a+b)*g(n,j-1)  
+        end do  
+   
+        call thomas_algorithm(a,b,c,d,JI,u_new)     
+        g(n+1,j)= g_novo(j)
+        end do        
 
         end program ferramentasmatematicas 
